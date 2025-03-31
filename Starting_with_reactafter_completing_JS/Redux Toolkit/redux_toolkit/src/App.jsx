11b732a6b24638b86365e709adf4db34a3a9548c
akshay <shettyakshay333@gmail.com>
2025-03-31 23:39:00 +0530

    redux_toolkit

--- Starting_with_reactafter_completing_JS/Redux Toolkit/redux_toolkit/src/App.jsx
+++ Starting_with_reactafter_completing_JS/Redux Toolkit/redux_toolkit/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
-
+import AddTodo from "./components/Addtodo";
+import Todos from "./components/Todos";
 function App() {
+  
+
   return (
     <>
       <h1>learn about redux tool kit</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }

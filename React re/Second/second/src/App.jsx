922c61ecd88a52929aa6226bbf3b143b9311f478
akshay <shettyakshay333@gmail.com>
2025-04-16 21:57:47 +0530

    Second_Session

---
+++ React re/Second/second/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Newcomponent from "./Newcomponent";
+function App() {
+  return (
+    <>
+      <h1> Understanding file structure of the React </h1>
+      <Newcomponent />
+    </>
+  );
+}
+
+export default App;
---
+++ React re/Second/second/src/Newcomponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Newcomponent() {
+  return (
+    <>
+      <h2> adding this jsx file in main page as a component </h2>
+      {/* // rules
+      // 1. Jsx file should start with capital 
+      // 2.  the file  extension should be   jsx   either wise  we cannot add jsx syntax in the  Js file  */}
+    </>
+  );
+}
+
+export default Newcomponent;
